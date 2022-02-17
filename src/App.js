4492ad4d2b63ea8cae93672b2134badec1a008e1
betteriq <anoushrs@gmail.com>
2022-02-17 03:33:17 -0500

    second-step

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import "./App.css";
 import About from "./components/About";
+import Shop from "./components/Shop";
+import Menu from "./components/Menu";
+import Client from "./components/Client";
+import Prices from "./components/Prices";
 const App = () => {
   return (
     <div>
       <Header />
-      <About/>
+      <About />
+      <Shop />
+      <Menu />
+      <Client />
+      <Prices />
     </div>
   );
 };
---
+++ src/components/Prices.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Prices = () => {
+  return (
+    <div className="prices">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <div className="price">
+              <div className="price_image">
+                <img src="./images/food_1.jpg" alt="" />
+              </div>
+              <h1 className="price_heading">Pizaa</h1>
+              <p className="price_text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+                aspernatur est aut error quos nisi consequuntur accusantium
+                ipsa? Dolor totam amet voluptate natus voluptatem qui quasi,
+                eligendi dolore fugiat quam.
+              </p>
+              <p className="price_rs">50$</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Prices;
