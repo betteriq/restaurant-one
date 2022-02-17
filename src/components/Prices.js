b1837ddb5369a8088b6eba4db42064915a4d6fb1
betteriq <anoushrs@gmail.com>
2022-02-17 05:15:33 -0500

    step-two

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -5,19 +5,64 @@ const Prices = () => {
     <div className="prices">
       <div className="container">
         <div className="row">
-          <div className="col-4">
+          <div className="col-3">
             <div className="price">
               <div className="price_image">
                 <img src="./images/food_1.jpg" alt="" />
               </div>
-              <h1 className="price_heading">Pizaa</h1>
+              <h1 className="price_heading">قیمه سیب زمینی</h1>
               <p className="price_text">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
                 aspernatur est aut error quos nisi consequuntur accusantium
                 ipsa? Dolor totam amet voluptate natus voluptatem qui quasi,
                 eligendi dolore fugiat quam.
               </p>
-              <p className="price_rs">50$</p>
+              <p className="price_rs">20000 تومان</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="price">
+              <div className="price_image">
+                <img src="./images/food_1.jpg" alt="" />
+              </div>
+              <h1 className="price_heading"> کباب کوبیده</h1>
+              <p className="price_text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+                aspernatur est aut error quos nisi consequuntur accusantium
+                ipsa? Dolor totam amet voluptate natus voluptatem qui quasi,
+                eligendi dolore fugiat quam.
+              </p>
+              <p className="price_rs">20000 تومان</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="price">
+              <div className="price_image">
+                <img src="./images/food_1.jpg" alt="" />
+              </div>
+              <h1 className="price_heading">قیمه</h1>
+              <p className="price_text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+                aspernatur est aut error quos nisi consequuntur accusantium
+                ipsa? Dolor totam amet voluptate natus voluptatem qui quasi,
+                eligendi dolore fugiat quam.
+              </p>
+              <p className="price_rs">20000 تومان</p>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="price">
+              <div className="price_image">
+                <img src="./images/food_1.jpg" alt="" />
+              </div>
+              <h1 className="price_heading">چلو مرغ</h1>
+              <p className="price_text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+                aspernatur est aut error quos nisi consequuntur accusantium
+                ipsa? Dolor totam amet voluptate natus voluptatem qui quasi,
+                eligendi dolore fugiat quam.
+              </p>
+              <p className="price_rs">20000 تومان</p>
             </div>
           </div>
         </div>
