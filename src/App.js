66902146867a110f00bd2c396fbee7b681b730d7
betteriq <anoushrs@gmail.com>
2022-02-17 01:43:17 -0500

    first-branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css";
+import About from "./components/About";
 const App = () => {
   return (
     <div>
       <Header />
+      <About/>
     </div>
   );
 };
